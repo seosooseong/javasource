b3cb4dd380edd3f861bc505ccc6da006b6067809
sooseong <tjtntjd82@naver.com>
2020-11-03 15:01:53 +0900

    소스수정

--- Exception/src/exception/exceptionTest2.java
+++ Exception/src/exception/exceptionTest2.java
@@ -3,8 +3,14 @@ package exception;
 public class exceptionTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		
+		String data1 = "100";
+		String data2 = "a100";
+		
+		int value1 = Integer.parseInt(data1);
+		int value2 = Integer.parseInt(data2); //런타임에러
+		
+		System.out.println(value1+value2);
 	}
 
 }
