aa0356aa00db065378d1b1f07914c8491b815307
sooseong <tjtntjd82@naver.com>
2020-11-03 15:16:03 +0900

    오류 제거 완료

--- Exception/src/exception/exceptionTest2.java
+++ Exception/src/exception/exceptionTest2.java
@@ -5,7 +5,7 @@ public class exceptionTest2 {
 	public static void main(String[] args) {
 		
 		String data1 = "100";
-		String data2 = "a100";
+		String data2 = "200";
 		
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2); //런타임에러-실행해야 알 수 있음.
