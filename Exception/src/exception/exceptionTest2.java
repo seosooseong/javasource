56e57e6d9ecb61f54d9f7ca675322e22299123b7
sooseong <tjtntjd82@naver.com>
2020-11-03 18:11:14 +0900

    commit 수정

--- Exception/src/exception/exceptionTest2.java
+++ Exception/src/exception/exceptionTest2.java
@@ -5,13 +5,14 @@ public class exceptionTest2 {
 	public static void main(String[] args) {
 		
 		String data1 = "100";
-		String data2 = "a100";
+		String data2 = "200";
 		
 		int value1 = Integer.parseInt(data1);
-		int value2 = Integer.parseInt(data2); //런타임에러-실행해야 알 수 있음.
+		int value2 = Integer.parseInt(data2);
 		
 		System.out.println(value1+value2);
 		
+		System.out.println(value1);
 		
 	}
 
