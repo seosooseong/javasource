6c803baa4cddf90ffae1d5bf533b1548ab0a750b
sooseong <tjtntjd82@naver.com>
2020-11-03 15:03:22 +0900

    소스-런타임 에러.

--- Exception/src/exception/exceptionTest2.java
+++ Exception/src/exception/exceptionTest2.java
@@ -8,7 +8,7 @@ public class exceptionTest2 {
 		String data2 = "a100";
 		
 		int value1 = Integer.parseInt(data1);
-		int value2 = Integer.parseInt(data2); //런타임에러
+		int value2 = Integer.parseInt(data2); //런타임에러-실행해야 알 수 있음.
 		
 		System.out.println(value1+value2);
 	}
