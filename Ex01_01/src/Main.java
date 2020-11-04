ccc5f3e79db476e714432ee4fc9c46ba1e41202e
seosooseong <tjtntjd82@naver.com>
2020-11-05 01:52:18 +0900

    Home push test 01

--- Ex01_01/src/Main.java
+++ Ex01_01/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("집에서 한건데 잘 됩니까?");
 		System.out.println("2차 테스트");
-		System.out.println("3차 test"); //pull용
+		System.out.println("3차 test"); 
+		
+		System.out.println("Home push test 01");
 	}
 
 }
